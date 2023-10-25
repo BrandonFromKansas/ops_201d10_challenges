6be26535229805b0ba273fa951dc0560238dcf34
BrandonFromKansas <brandon.l.jensen@gmail.com>
2023-10-24 19:29:20 -0700

    Here is my first Ops Challenge submission.

--- my-first-ops-challenge.sh
+++ my-first-ops-challenge.sh
@@ -1,1 +1,17 @@
-# This is our first ACP workflow
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Brandon Jensen - I am using the template that Marco used during lecture today and just replicating exactly what he showed us.
+# Date of latest revision:      10/24/2023
+# Purpose:                      Print a phrase to the screen.
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+echo "Hello World"
+
+# End
